f2fd12b6e913cba1b6a0bc56c9e8d223a903de1c
Anurag-Band <anuragband10@gmail.com>
2025-10-12 14:49:21 +0530

    feat: initialised next.js project with tailwind css using bun & template cleanup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center flex-col min-h-screen">
+      <h1 className="">Anurag Band...</h1>
+      <h2>See my work</h2>
+    </div>
+  );
+}
+
